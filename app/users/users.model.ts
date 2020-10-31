537e1bea553d92c7936ea6abc7eb20261fb8b893
tomas.azevedo <tombotA1985>
2020-10-31 12:01:00 -0300

    API de usuários (parte 2).

--- app/users/users.model.ts
+++ app/users/users.model.ts
@@ -1,15 +1,52 @@
+// Fake for while
 const users = [
-    {name:'Peter Parker', email: 'peter@marvel.com'},
-    {name:'Bruce Wayne', email: 'bruce@dc.com'}
+    {id: '1', name:'Peter Parker', email: 'peter@marvel.com'},
+    {id: '2', name:'Bruce Wayne', email: 'bruce@dc.com'}
 ]
 
+/**
+ *
+ * Classe que representa um usuário do sistema.
+ *
+ */
 export class User {
 
 
-    static findAll(): Promise<any[]>{
+    /**
+     *
+     * Método que retorna todos os usuários.
+     *
+     */
+    static findAll(): Promise<any[]> {
 
         return Promise.resolve(users)
 
     }
+
+
+    /**
+     *
+     * Método que retorna um determinado usuário pelo número do id.
+     *
+     * @param id - número identificador do usuário.
+     *
+     */
+    static findById(id: string): Promise<any> {
+
+        return new Promise(resolve => {
+
+            const filtered = users.filter(user => user.id === id)
+
+            let user = undefined
+            if (filtered.length > 0) {
+                user = filtered[0]
+            }
+
+            resolve(user)
+
+        })
+
+    }
+
 
 }
--- app/users/users.router.ts
+++ app/users/users.router.ts
@@ -2,16 +2,54 @@ import {Router} from '../common/router'
 import * as restify from 'restify'
 import {User} from './users.model'
 
+/**
+ *
+ * Classe responsável pelas operações do recurso User.
+ *
+ */
 class UsersRouter extends Router {
 
     applyRoutes(application: restify.Server) {
 
+
+        /**
+         *
+         * Método GET - /users
+         *
+         */
         application.get('/users', (req, resp, next) => {
+
             User.findAll().then(users => {
+
                 resp.json(users)
                 return next()
+
             })
+
         })
+
+
+        /**
+         *
+         * Método GET - /users:id
+         *
+         */
+        application.get('/users/:id', (req, resp, next) => {
+
+            User.findById(req.params.id).then(user => {
+
+                if (user) {
+                    resp.json(user)
+                    return next()
+                }
+
+                resp.send(404)
+                return next()
+
+            })
+
+        })
+
 
     }
 
