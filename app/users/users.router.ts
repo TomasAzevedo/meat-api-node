7a0a7450bc8db5b0032add7fbeac1f632430ec01
tomas.azevedo <tombotA1985>
2020-11-07 10:08:05 -0300

    API de usuários (parte 3).

--- app/users/users.router.ts
+++ app/users/users.router.ts
@@ -9,6 +9,17 @@ import {User} from './users.model'
  */
 class UsersRouter extends Router {
 
+    constructor() {
+
+        super()
+
+        this.on('beforeRender', document => {
+            document.password = undefined
+            //delete document.password
+        })
+
+    }
+
     applyRoutes(application: restify.Server) {
 
 
@@ -19,13 +30,8 @@ class UsersRouter extends Router {
          */
         application.get('/users', (req, resp, next) => {
 
-            User.find().then(users => {
-
-                resp.json(users)
-                console.log('asaaa')
-                return next()
-
-            })
+            User.find()
+                .then(this.render(resp,next))
 
         })
 
@@ -37,17 +43,8 @@ class UsersRouter extends Router {
          */
         application.get('/users/:id', (req, resp, next) => {
 
-            User.findById(req.params.id).then(user => {
-
-                if (user) {
-                    resp.json(user)
-                    return next()
-                }
-
-                resp.send(404)
-                return next()
-
-            })
+            User.findById(req.params.id)
+                .then(this.render(resp,next))
 
         })
 
@@ -61,10 +58,64 @@ class UsersRouter extends Router {
 
             let user = new User(req.body)
 
-            user.save().then(user => {
+            user.save()
+                .then(this.render(resp,next))
 
-                user.password = undefined
-                resp.json(user)
+        })
+
+
+        /**
+         *
+         * Método PUT - /users/:id
+         *
+         */
+        application.put('/users/:id', (req, resp, next) => {
+
+            const options = {overwrite: true}
+
+            User.update({_id: req.params.id}, req.body, options).exec().then(result => {
+
+                if (result.n) {
+                    return User.findById(req.params.id)
+                } else {
+                    resp.send(404)
+                }
+
+            }).then(this.render(resp,next))
+
+        })
+
+
+        /**
+         *
+         * Método PATCH - /users/:id
+         *
+         */
+        application.patch('/users/:id', (req, resp, next) => {
+
+            const options = {new: true}
+
+            User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp,next))
+
+        })
+
+
+        /**
+         *
+         * Método DELETE - /users/:id
+         *
+         */
+        application.del('/users/:id', (req, resp, next) => {
+
+            User.deleteOne({_id: req.params.id}).exec().then((result: any) => {
+
+                if (result.n) {
+                    resp.send(204)
+                } else {
+                    resp.send(404)
+                }
+
                 return next()
 
             })
