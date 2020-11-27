865343d65fd37e5464c3cae9662bed2551982952
tomas.azevedo <tombotA1985>
2020-11-27 12:43:15 -0300

    Adicionando abstração nos routers.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Aplicação baseada no curso da Udemy de Tarso Bessa (Do Zero à Nuvem: API Restful com NodeJS, Restify e MongoDB).
 
 https://www.udemy.com/course/nodejs-rest-pt/
+
+## Para rodar local
+
+```bash
+git clone ...
+npm install
+nodemon dist/main.js
+mongod
+```
+
--- app/users/users.router.ts
+++ app/users/users.router.ts
@@ -1,17 +1,18 @@
 import {Router} from '../common/router'
 import * as restify from 'restify'
 import {User} from './users.model'
+import {ModelRouter} from "../common/model-router";
 
 /**
  *
  * Classe responsável pelas operações do recurso User.
  *
  */
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
 
     constructor() {
 
-        super()
+        super(User)
 
         this.on('beforeRender', document => {
             document.password = undefined
@@ -22,111 +23,12 @@ class UsersRouter extends Router {
 
     applyRoutes(application: restify.Server) {
 
-
-        /**
-         *
-         * Método GET - /users
-         *
-         */
-        application.get('/users', (req, resp, next) => {
-
-            User.find()
-                .then(this.render(resp,next))
-                .catch(next)
-
-        })
-
-
-        /**
-         *
-         * Método GET - /users:id
-         *
-         */
-        application.get('/users/:id', (req, resp, next) => {
-
-            User.findById(req.params.id)
-                .then(this.render(resp,next))
-                .catch(next)
-
-        })
-
-
-        /**
-         *
-         * Método POST - /users
-         *
-         */
-        application.post('/users', (req, resp, next) => {
-
-            let user = new User(req.body)
-
-            user.save()
-                .then(this.render(resp,next))
-                .catch(next)
-
-        })
-
-
-        /**
-         *
-         * Método PUT - /users/:id
-         *
-         */
-        application.put('/users/:id', (req, resp, next) => {
-
-            const options = {runValidators: true, overwrite: true}
-
-            User.update({_id: req.params.id}, req.body, options).exec().then(result => {
-
-                if (result.n) {
-                    return User.findById(req.params.id)
-                } else {
-                    resp.send(404)
-                }
-
-            }).then(this.render(resp,next))
-              .catch(next)
-
-        })
-
-
-        /**
-         *
-         * Método PATCH - /users/:id
-         *
-         */
-        application.patch('/users/:id', (req, resp, next) => {
-
-            const options = {runValidators: true, new: true}
-
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp,next))
-                .catch(next)
-
-        })
-
-
-        /**
-         *
-         * Método DELETE - /users/:id
-         *
-         */
-        application.del('/users/:id', (req, resp, next) => {
-
-            User.deleteOne({_id: req.params.id}).exec().then((result: any) => {
-
-                if (result.n) {
-                    resp.send(204)
-                } else {
-                    resp.send(404)
-                }
-
-                return next()
-
-            }).catch(next)
-
-        })
-
+        application.get('/users', this.findAll)
+        application.get('/users/:id', [this.validateId, this.findById])
+        application.post('/users', this.save)
+        application.put('/users/:id', [this.validateId,this.replace])
+        application.patch('/users/:id', [this.validateId,this.update])
+        application.del('/users/:id', [this.validateId,this.delete])
 
     }
 
