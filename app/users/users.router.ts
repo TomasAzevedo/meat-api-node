85769e01cffa25b5e5b6060d6239fd91f52ad617
tomas.azevedo <tombotA1985>
2020-12-12 09:35:30 -0300

    Versionamento na API de Users.

--- app/users/users.router.ts
+++ app/users/users.router.ts
@@ -10,6 +10,7 @@ import {ModelRouter} from "../common/model-router";
  */
 class UsersRouter extends ModelRouter<User> {
 
+
     constructor() {
 
         super(User)
@@ -21,16 +22,33 @@ class UsersRouter extends ModelRouter<User> {
 
     }
 
+
+    findByEmail = (req, resp, next) => {
+        if (req.query.email) {
+            User.find({email: req.query.email})
+                .then(this.renderAll(resp, next))
+                .catch(next)
+        } else {
+            next()
+        }
+    }
+
+
     applyRoutes(application: restify.Server) {
 
-        application.get('/users', this.findAll)
+        application.get('/users',restify.plugins.conditionalHandler([
+            { version: '1.0.0', handler: this.findAll },
+            { version: '2.0.0', handler: [this.findByEmail, this.findAll] }
+        ]));
+        //application.get('/users', this.findAll)
         application.get('/users/:id', [this.validateId, this.findById])
         application.post('/users', this.save)
-        application.put('/users/:id', [this.validateId,this.replace])
-        application.patch('/users/:id', [this.validateId,this.update])
-        application.del('/users/:id', [this.validateId,this.delete])
+        application.put('/users/:id', [this.validateId, this.replace])
+        application.patch('/users/:id', [this.validateId, this.update])
+        application.del('/users/:id', [this.validateId, this.delete])
 
     }
+
 
 }
 
