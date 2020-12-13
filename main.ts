44aad6ca23a25e61a5d2821d4824b7685550a9ed
tomas.azevedo <tombotA1985>
2020-12-13 09:46:26 -0300

    Ajuste no código.

--- main.ts
+++ main.ts
@@ -13,14 +13,10 @@ server.bootstrap([
         mainRouter
     ])
     .then(server => {
-
         console.log('Server is listening on:', server.application.address())
-
     }).catch(error => {
-
         console.log('Server failed to start')
         console.error(error)
         process.exit(1)
-
     }
 )
